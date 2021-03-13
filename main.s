461420730e9d53fe48d1b37a1d522b677c978417
dagurasu-san <hanadouglas@gmail.com>
2021-03-14 02:27:03 +0800

    Completing LCD file, need to figure out how to refer to psect data table loc

--- main.s
+++ main.s
@@ -1,13 +1,9 @@
 #include <xc.inc>
 
-<<<<<<< OURS
 extrn	LCDSetup, LCDWrite	; external LCD subroutines
 extrn	intKey, keypadSetup, bigDelay
-=======
 ;extrn	LCDSetup, LCD_Write_Message, LCD_Write_Hex ; external LCD subroutines
-extrn	intKey, keypadSetup
->>>>>>> THEIRS
-    
+;extrn	intKey, keypadSetup
    
 psect	udata_acs   ; reserve data space in access ram
 storedKey:	ds 4    ; reserve 4 bytes for 4 digit stored keycode
@@ -19,19 +15,17 @@ attemptTimerHigh:	ds 1	; reserve 1 byte for the timer.  It is likely this
 attemptTimerLow:	ds 1
 			
 
-			
-
 psect	code, abs	
 init: 	org	0x00
  	goto	setup
 
-intHigh:	
-	org	 0x0008		; high interrupt triggered by keypad input
-<<<<<<< OURS
-	goto	 intKey	; store keypad input
-=======
-	goto	 intKey		; store keypad input
->>>>>>> THEIRS
+;intHigh:	
+;	org	 0x0008		; high interrupt triggered by keypad input
+;<<<<<<< OURS
+;	goto	 intKey	; store keypad input
+;=======
+;	goto	 intKey		; store keypad input
+;>>>>>>> THEIRS
 
 ;=======Setup I/O===============================================================
 
@@ -59,113 +53,115 @@ setup:
 start: 
 	; default 
 	; display "Please enter passcode" or something 
-<<<<<<< OURS
-	call	intKey
+	movlw	0
 	call	LCDWrite
-	call	bigDelay
-	goto	start
-=======
-	goto	mainLoop
+;	call	bigDelay
+;	call	bigDelay
+;	call	bigDelay
+;	movlw	0
+;	call	LCDWrite
+	goto	$
+;	goto	mainLoop
 	
-mainLoop:
-	;check code counter
-	;if code counter = 4, do the code checking sequence
-	movlw	0x04
-	cpfslt	codeCounter
-	;4 digits have been entered, so we do the next line
-	goto	checkCode
-	;4 digits have not been entered, so we do the next line
-	movlw	0x00
-	; check to see if the attempt timer has run out
-	cpfsgt	attemptTimer
-	; the code counter is reset if the timer has run out.
-	call	resetCodeCounter
-	; now we decrement the timer if the contents of the timer 
-	; are greater than 0
-	cpfslt	attemptTimer
-	call	decrementAttemptTimer
-	;now loop back to the beginning
-	goto	mainLoop
+;mainLoop:
+;	;check code counter
+;	;if code counter = 4, do the code checking sequence
+;	movlw	0x04
+;	cpfslt	codeCounter
+;	;4 digits have been entered, so we do the next line
+;	goto	checkCode
+;	;4 digits have not been entered, so we do the next line
+;	movlw	0x00
+;	; check to see if the attempt timer has run out
+;	cpfsgt	attemptTimer
+;	; the code counter is reset if the timer has run out.
+;	call	resetCodeCounter
+;	; now we decrement the timer if the contents of the timer 
+;	; are greater than 0
+;	cpfslt	attemptTimer
+;	call	decrementAttemptTimer
+;	;now loop back to the beginning
+;	goto	mainLoop
 	
-	
-checkCode:
-	; At this point, 4 digits of the code have been entered.
-	; We must now check to see if they are the correct values
->>>>>>> THEIRS
-	
-compareKey: 
-	; after 4 digits entered it will come here via goto 
-	; if wrong display error message and goto start
-	
-appendEnteredCode:
-    ;	Append the code that has been entered into the keypad
-    ;	assume w register contains the new code
-	movwf	givenKey + codeCounter ; how?
-	return
-
-resetCodeCounter:
-	; reset the value of the actual counter
-	movlw	0x00
-	movwf	codeCounter
-	; FSR0 is used to store the value of the desired memory location
-	; for the input digits, and should be reset to the first location 
-	; now
-	lfsr	0, givenKey
-	
-	return
-	
-	
-; timer related code
-resetAttemptTimer:
-	movlw	0xFF ;high(0xFFFF)
-	movwf	attemptTimerHigh, A
-	movlw	0xFF ;low(0xFFFF)
-	movwf	attemptTimerLow, A
-	
-decrementAttemptTimer:
-	movlw	0x01
-	cpfslt	attemptTimerLow, A
-	bra	timerA
-	bra	timerB
-timerA:
-	movlw	0xFF
-timerB:
-	
-	return
-	
-
-	
-    
-delay16_repeater:
-	; This calls the sixteen-bit delay FFh times.
-	movlw	0xFF
-	movwf	delay16_repeats, A
-delay16_repeater_loop:
-	call	delay16
-	decfsz	delay16_repeats, A
-	bra	delay16_repeater_loop
-	return	
-	
-delay16:
-	
-	movlw	0x00
-dloop:	
-	; decrement your way through the 16 bits
-	decf	delay16_counterLow, f, A
-	subwfb	delay16_counterHigh, f, A
-	bc	dLoop
-	return
-	
-; lock related code
-	
-lock:	; This sends the signal to lock the lock
-	movlw	0xFF
-	movwf	PORTC, A
-	return
-	
-unlock:	; This sends the signal to unlock the lock
-	movlw	0x00
-	movwf	PORTC, A
-	return
-    
+;	
+;checkCode:
+;	; At this point, 4 digits of the code have been entered.
+;	; We must now check to see if they are the correct values
+;>>>>>>> THEIRS
+;	
+;compareKey: 
+;	; after 4 digits entered it will come here via goto 
+;	; if wrong display error message and goto start
+;	
+;appendEnteredCode:
+;    ;	Append the code that has been entered into the keypad
+;    ;	assume w register contains the new code
+;	movwf	givenKey + codeCounter ; how?
+;	return
+;
+;resetCodeCounter:
+;	; reset the value of the actual counter
+;	movlw	0x00
+;	movwf	codeCounter
+;	; FSR0 is used to store the value of the desired memory location
+;	; for the input digits, and should be reset to the first location 
+;	; now
+;	lfsr	0, givenKey
+;	
+;	return
+;	
+;	
+;; timer related code
+;resetAttemptTimer:
+;	movlw	0xFF ;high(0xFFFF)
+;	movwf	attemptTimerHigh, A
+;	movlw	0xFF ;low(0xFFFF)
+;	movwf	attemptTimerLow, A
+;	
+;decrementAttemptTimer:
+;	movlw	0x01
+;	cpfslt	attemptTimerLow, A
+;	bra	timerA
+;	bra	timerB
+;timerA:
+;	movlw	0xFF
+;timerB:
+;	
+;	return
+;	
+;
+;	
+;    
+;delay16_repeater:
+;	; This calls the sixteen-bit delay FFh times.
+;	movlw	0xFF
+;	movwf	delay16_repeats, A
+;delay16_repeater_loop:
+;	call	delay16
+;	decfsz	delay16_repeats, A
+;	bra	delay16_repeater_loop
+;	return	
+;	
+;delay16:
+;	
+;	movlw	0x00
+;dloop:	
+;	; decrement your way through the 16 bits
+;	decf	delay16_counterLow, f, A
+;	subwfb	delay16_counterHigh, f, A
+;	bc	dLoop
+;	return
+;	
+;; lock related code
+;	
+;lock:	; This sends the signal to lock the lock
+;	movlw	0xFF
+;	movwf	PORTC, A
+;	return
+;	
+;unlock:	; This sends the signal to unlock the lock
+;	movlw	0x00
+;	movwf	PORTC, A
+;	return
+;    
     end	init
