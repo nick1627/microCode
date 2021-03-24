5552549955f2c2de1b0dc82627c7394d18e60ad5
dagurasu-san <hanadouglas@gmail.com>
2021-03-24 15:02:27 +0000

    Cleaning up EEPROM.s

--- EEPROM.s
+++ EEPROM.s
@@ -10,56 +10,49 @@ psect	EEPROM_code, class=CODE	;===============================================
 
 readEEPROM: 
     ; read data in EEPROM (@EEAddr) to storedKey	    
-	; specify EEPROM address to be written-in with EEADR
-	; banksel	EECON1			; select EEPROM memory bank 
-	clrf	EEADR, A		; reset set address in EEPROM at 0x0000
+	; specify EEPROM address to be read from with EEADR
+	clrf	EEADR, A		; point to address in EEPROM at 0x0000
 	clrf	EEADRH, A
 	
 	; setup EEPROM for reading 
-	clrf	EECON1, A		; clears EEPGD/CFGS to select EEPROM memory
+	clrf	EECON1, A		; clears EEPGD/CFGS to select EE memory
 
-	; read EEADR to EEADR+4 into storedKey in FSR1
-	lfsr	1, storedKey		; load FSR1 with storedKey memory location -- CHANGE TEST KEY
+	; read EEADR to EEADR+3 into storedKey at FSR1
+	lfsr	1, storedKey		; load FSR1 with storedKey location
 	movlw	codeLength
-	movwf	EECounter, A		; load counter with the number of keys (4)
-	; incf	EEADR, A		; read/write begins at location: 0x0001
+	movwf	EECounter, A		; load counter with the number of keys
 
 readLp:	; loop to read data for each key 
 	bsf	RD			; initialise read cycle 
 	nop				; leave one cycle for reading 
-;	movf	EEDATA, W, A		; move data to location indicated by FSR1 (storedKey)
-;	movwf	POSTINC1, A
+	movff	EEDATA, POSTINC1, A	; move data to storedKey pointed by FSR1
 	
-	movff	EEDATA, POSTINC1, A
-	
-	incf	EEADR, A		; increment EEADR to the next location in EE
-	decfsz	EECounter, A
-	bra	readLp			; repeat for all (4) keys
+	incf	EEADR, A		; move to the next location in EE
+	decfsz	EECounter, A		; decrement counter and
+	bra	readLp			;	repeat for all keys
 	
 	; reset system and return 
-	bsf	EEPGD			; reset EEPGD to select flash data memory 
+	bsf	EEPGD			; set EEPGD to select flash data memory 
 	return
 	
 writeEEPROM:
     ; write data from storedKey to EEPROM (@EEAddr)
 	; specify EEPROM address to be written-in with EEADR
-	; banksel	EECON1		    ; select EEPROM memory bank
-	clrf	EEADR, A		    ; reset set address in EEPROM at 0x0000
+	clrf	EEADR, A		; point to address in EEPROM at 0x0000
 	clrf	EEADRH, A
 	
 	; setup EEPROM for writing 
-	clrf	EECON1, A		    ; clears EEPGD/CFGS to select EEPROM memory
-	bsf	WREN		    ; set WREN to enable writing to EEPROM 
-	bcf	GIE		    ; disable interrupt whilst writing
+	clrf	EECON1, A		; clears EEPGD/CFGS to select EE memory
+	bsf	WREN			; set WREN to enable writing to EEPROM 
+	bcf	GIE			; disable interrupt whilst writing
 
-	; write storedKey in FSR1 into EEADR to EEADR+4
-	lfsr	1, storedKey	    ; load FSR1 with storedKey memory location
+	; write storedKey in FSR1 into EEADR to EEADR+3
+	lfsr	1, storedKey		; load FSR1 with storedKey location
 	movlw	codeLength
-	movwf	EECounter, A	    ; load counter with the number of keys (4)
-	; incf	EEADR, A		    ; read/write begins at location: 0x0001
+	movwf	EECounter, A		; load counter with the number of keys
 	
 writeLp:; loop to write data for ecah key 
-	movf	POSTINC1, W, A	    ; move 8-bit data from storedKey to EEDATA
+	movf	POSTINC1, W, A		; move data from storedKey to EEDATA
 	movwf	EEDATA, A		    
 	
 	movlw	0x55		    ; required sequence for writing to EEPROM
@@ -83,31 +76,3 @@ writeLp:; loop to write data for ecah key
 	bsf	EEPGD		; reset EEPGD to select flash data memory 
 
 	return 
-	
-	
-	
-;;resetEEPROM: 
-;;	clrf	EEADR
-;;	clrf	EEADRH
-;;	bcf	EECON1, 6
-;;	bcf	EECON1, 7
-;;	bcf	GIE
-;;	bsf	EECON1, 2
-;;resetLp:	
-;;	bsf	EECON1, 0
-;;	movlw	0x55
-;;	movwf	EECON2
-;;	movlw	0xAA
-;;	movwf	EECON2
-;;	bsf	EECON1, 1
-;;	btfsc	EECON1, 1
-;;	bra	$-2
-;;	incfsz	EEADR, F
-;;	bra	resetLp 
-;;	incfsz	EEADRH, F
-;;	bra	resetLp
-;;	
-;;	bcf	EECON1, 2
-;;	bsf	GIE
-;;	
-;;	return 
